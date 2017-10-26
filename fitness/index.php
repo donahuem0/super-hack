c9a5c9b9b06252199f9b3453803d7b3767e788d2
CarltonM <carltonmugo@gmail.com>
2017-10-26 10:11:58 -0400

    Update index.php

--- fitness/index.php
+++ fitness/index.php
@@ -1,11 +1,14 @@
 <?php 
 
+$txt = "Helloe World!";
+
 get_header(); 
 
 
   if ( have_posts() ):
     while( have_posts() ):
       the_post(); 
+      
       
       get_template_part( 'content-templates/content', get_post_format() );
       
